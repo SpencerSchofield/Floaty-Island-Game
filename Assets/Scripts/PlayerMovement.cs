d88c3a0c7ec507707d7754285e15100adcbdd672
Spencer <spencerschofield224@gmail.com>
2023-06-21 17:29:34 +0100

    Added slope movement
    player can now better move along slopes ( however exiting a slope is still bugged - character doesn't stay on slope when walking down it)
    Added a Test scene to better test player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,29 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+	
 	[Header("Movement")]
-	public float moveSpeed;
+	private float moveSpeed;
+	public float walkSpeed;
+	public float sprintSpeed;
 	public float groundDrag;
 
-	
 	public float jumpForce;
 	public float jumpCooldown;
 	public float airMultiplier;
 	public bool readyToJump = true;
+	public bool exitingSlope;
 	
+	[Header("KeyBinds")]
 	public KeyCode jumpKey = KeyCode.Space;
-  public KeyCode sprintKey =KeyCode.LeftShift;
+  	public KeyCode sprintKey = KeyCode.LeftShift;
 	
 	[Header("Ground Check")]
 	public float playerHeight;
 	public LayerMask whatIsGround;
 	bool grounded;
 	
+	[Header("Slope Handling")]
+	public float maxSlopeAngle;
+	private RaycastHit slopeHit;
+	
 	public Transform orientation;
 	float horizontalInput;
 	float verticalInput;
 	Vector3 moveDirection;
 	Rigidbody rb;
+	public MovementState state;
+	public enum MovementState
+	{
+		walking,
+		sprinting,
+		air
+	}
 	
 	void Start()
 	{
@@ -52,6 +67,28 @@ public class PlayerMovement : MonoBehaviour
 		}
 				
 	}
+	
+	private void StateHandler()
+	{
+		//Mode - Sprinting
+		if(grounded && Input.GetKey(sprintKey))
+		{
+			state = MovementState.sprinting;
+			moveSpeed = sprintSpeed;
+		}
+		//Mode - Walking
+		else if (grounded)
+		{
+			state = MovementState.walking;
+			moveSpeed = walkSpeed;
+		}
+		//Mode - Air
+		else
+		{
+			state = MovementState.air;
+		}
+	}
+	
 	/*
 	MovePlayer() function handles the direction the player is moving, it also allows me to change the speed they walk
 	while on the floor and the speed they have while jumping / falling in air
@@ -61,8 +98,19 @@ public class PlayerMovement : MonoBehaviour
 		//calculate movement direction
 		moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 		
+		//on slope
+		if(OnSlope() && exitingSlope)
+		{
+			rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
+			
+			if(rb.velocity.y > 0)
+			{
+				rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+			}
+		}
+		
 		//on ground
-		if(grounded){
+		else if(grounded){
 		rb.AddForce(moveDirection.normalized * moveSpeed*10f,ForceMode.Force);
 		}
 		//in air
@@ -71,12 +119,9 @@ public class PlayerMovement : MonoBehaviour
 			rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
 
 		}
-		
-		if(Input.GetKey(sprintKey) && grounded)
-		{
-			rb.AddForce(moveDirection.normalized * moveSpeed*20f,ForceMode.Force);
 
-		}
+		//turn off gravity when on slope
+		rb.useGravity = !OnSlope();
 		
 		
 	}
@@ -86,13 +131,24 @@ public class PlayerMovement : MonoBehaviour
 	*/
 	private void SpeedControl()
 	{
-		Vector3 flatVal = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+		//limiting speed on slope
+		if(OnSlope() && !exitingSlope)
+		{
+			if(rb.velocity.magnitude > moveSpeed){
+			rb.velocity = rb.velocity.normalized * moveSpeed;
+		} 
+		}
+		//limiting speed on ground or air
+		else 
+		{
+			Vector3 flatVal = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 		
 		if(flatVal.magnitude > moveSpeed){
 			Vector3 limitedVal = flatVal.normalized * moveSpeed;
 			rb.velocity = new Vector3(limitedVal.x, rb.velocity.y, limitedVal.z);
-			//Debug.Log("Speed Capped");
+		} 
 		}
+		
 		
 	}
 	
@@ -101,6 +157,7 @@ public class PlayerMovement : MonoBehaviour
 	*/
 	private void Jump()
 	{
+		exitingSlope = true;
 		//rest y velocity
 		rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 		rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
@@ -114,8 +171,26 @@ public class PlayerMovement : MonoBehaviour
 	private void ResetJump()
 	{
 		readyToJump = true;
+		exitingSlope= false;
 	}
 
+	private bool OnSlope()
+	{
+		if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f))
+		{
+			float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+			Debug.Log("On Slope");
+			Debug.Log(angle);
+			return angle < maxSlopeAngle && angle != 0;
+			
+		}
+		return false;
+	}
+	
+	private Vector3 GetSlopeMoveDirection()
+	{
+		return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
+	}
 
 	/*
 	Update() is called once per frame
@@ -127,6 +202,9 @@ public class PlayerMovement : MonoBehaviour
 		Debug.Log(grounded);
 		MyInput();
 		SpeedControl();
+		StateHandler();
+		
+		//handle drag
 		if(grounded)
 		{
 			rb.drag = groundDrag;
@@ -135,6 +213,10 @@ public class PlayerMovement : MonoBehaviour
 		{
 			rb.drag = 0;
 		}
+		
+		Debug.Log("Speed: " + rb.velocity);
+		Debug.Log("Speedo: " + rb.position.magnitude);
+		
 	}
 	
 	/*
