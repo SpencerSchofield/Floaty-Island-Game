e9329cb7988fe1b0e137d992b580cdf5cede43b7
Spencer <spencerschofield224@gmail.com>
2023-06-25 14:13:10 +0100

    Made air force weaker
    Made air force weaker, Player no longer sticks to walls when they should be falling

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -126,7 +126,7 @@ public class PlayerMovement : MonoBehaviour
 		//in air
 		else if(!grounded)
 		{
-			rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+			rb.AddForce(moveDirection.normalized * moveSpeed * 5f * airMultiplier, ForceMode.Force);
 
 		}
 
